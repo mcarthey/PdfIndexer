1e7c5c393fabd8e925c35754e4bb411191446ed4
Mark McArthey <mcarthey@gmail.com>
2024-07-17 18:08:30 -0500

    Add project files.

---
+++ Models/Document.cs
@@ -0,0 +1,7 @@
+﻿public class Document
+{
+    public int Id { get; set; }
+    public string FileName { get; set; }
+    public string Content { get; set; }
+    public DateTime LastModified { get; set; } 
+}
